b35e117ff3437f817fecb94137edddd3dee816a1
eonj <noreply@aurynj.net>
2022-01-14 17:59:38 +0900

    `api` from Spring Initializr, `ui` from CRA
    Spring Initializr:
    https://start.spring.io/
    Spring Boot 2.6.2, Kotlin/JVM (Java 8), Gradle (Kotlin DSL)
    com.example.just.api (just-example-api)
    Sample code layout downloaded and unzipped
    
    CRA:
    https://create-react-app.dev/
    create-react-app 5.0.0, cra-template-typscript 1.1.3
    Delivered here via: yarn create react-app just-example-ui --template typescript

---
+++ api/src/test/kotlin/com/example/just/api/JustExampleApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.just.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JustExampleApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
